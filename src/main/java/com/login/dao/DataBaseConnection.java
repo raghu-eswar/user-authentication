85d256b0323f1329a4dcfdb95f51697c2d5e91c2
raghueswar55 <raghueswar55@gmail.com>
2020-06-20 11:13:28 +0530

    Implemented jdbc connection to mysql database.

---
+++ src/main/java/com/login/dao/DataBaseConnection.java
@@ -0,0 +1,26 @@
+package com.login.dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DataBaseConnection {
+
+    private static final String URL = "jdbc:mysql://localhost:3306/login";
+    private static final String USER_NAME = "root";
+    private static final String PASSWORD = "raghu@123";
+    private static Connection connection;
+
+    public static Connection getConnection() {
+        if (connection == null) {
+            try {
+                Class.forName("com.mysql.cj.jdbc.Driver");
+                connection = DriverManager.getConnection(URL, USER_NAME, PASSWORD);
+            } catch (ClassNotFoundException | SQLException e) {
+                e.printStackTrace();
+            }
+        }
+        return connection;
+    }
+
+}
